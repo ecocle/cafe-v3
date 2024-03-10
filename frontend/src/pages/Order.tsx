30d724a3598ac767c44ee0e45cfbf976b2aa561f
ecocle <146603117+ecocle@users.noreply.github.com>
2024-03-10 20:27:35 +0800

    removed realy scuffed deals

--- frontend/src/pages/Order.tsx
+++ frontend/src/pages/Order.tsx
@@ -70,8 +70,6 @@ const Order = ({ itemType }: { itemType: string }) => {
     const newItemDetails = localStorage.getItem("orderDetails");
     const newItemAddedAmount = localStorage.getItem("addedAmount");
     const itemName = hashParams.get("name") ?? "";
-    const [showOptions, setShowOptions] = useState(false);
-    const [selectedAddition, setSelectedAddition] = useState("");
     const [itemDetails, setItemDetails] = useState<ItemDetails>({
         price: 0,
         hot: false,
@@ -303,25 +301,6 @@ const Order = ({ itemType }: { itemType: string }) => {
             useCup: value.useCup,
         };
 
-        if (
-            (itemName === "Rose Velvet Latte" && selectedAddition === "") ||
-            (itemName === "Tropical Rosy Fairytale" && selectedAddition === "")
-        ) {
-            localStorage.setItem("orderDetails", JSON.stringify(orderDetails));
-            setShowOptions(true);
-            return;
-        }
-
-        if (selectedAddition === "5") {
-            localStorage.setItem("addedAmount", "5");
-            navigate("/");
-            return;
-        } else if (selectedAddition === "8") {
-            localStorage.setItem("addedAmount", "8");
-            navigate("/");
-            return;
-        }
-
         try {
             const response = await fetch(`${baseUrl}/api/order`, {
                 method: "POST",
@@ -331,25 +310,6 @@ const Order = ({ itemType }: { itemType: string }) => {
                 },
                 body: JSON.stringify(orderDetails),
             });
-
-            if (newItemDetails) {
-                const response2 = await fetch(`${baseUrl}/api/order`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${token}`,
-                    },
-                    body: newItemDetails,
-                });
-
-                if (!response2.ok) {
-                    console.error("Error placing order");
-                    setIsLoading(false);
-                }
-
-                localStorage.removeItem("orderDetails");
-                localStorage.removeItem("addedAmount");
-            }
 
             if (!response.ok) {
                 console.error("Error placing order");
@@ -614,34 +574,6 @@ const Order = ({ itemType }: { itemType: string }) => {
                                         <span>Place Order</span>
                                     )}
                                 </Button>
-                                {showOptions && (
-                                    <div>
-                                        <Button
-                                            type="submit"
-                                            onClick={() =>
-                                                setSelectedAddition("0")
-                                            }
-                                        >
-                                            Order normally
-                                        </Button>
-                                        <Button
-                                            type="submit"
-                                            onClick={() =>
-                                                setSelectedAddition("5")
-                                            }
-                                        >
-                                            Plus ¥5 for free medium drink
-                                        </Button>
-                                        <Button
-                                            type="submit"
-                                            onClick={() =>
-                                                setSelectedAddition("8")
-                                            }
-                                        >
-                                            Plus ¥8 for free large drink
-                                        </Button>
-                                    </div>
-                                )}
                                 {hasPriceError && (
                                     <p className="text-destructive">
                                         Not enough fund in account
